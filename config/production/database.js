8c862f025e3a0c83154b010724579f1cbcb94c78
slava <slava@pop-os.localdomain>
2021-08-22 16:07:19 -0400

    changes in config

---
+++ config/production/database.js
@@ -0,0 +1,21 @@
+module.export = ({env}) => {
+  return {
+      defaultConnection: "default",
+      connections:{
+          default:{
+              connector: "bookshelf",
+              settings: {
+                  client: 'mysql',
+                  host: env('DATABASE_HOST', 'localhost'),
+                  port: env.int('DATABASE_PORT', 3306),
+                  database: env('DATABASE_NAME', 'strapi'),
+                  username: env('DATABASE_USERNAME', 'strapi'),
+                  password: env('DATABASE_PASSWORD', 'strapi'),
+                },
+              options:{
+                  ssl: false,
+              }
+          }
+      }
+  }
+}
---
+++ config/production/server.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url:env("HEROKU_URL"),
+};
+ 
